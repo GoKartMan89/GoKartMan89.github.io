d7876e225df16aed6634cc7b87d5c0a8e1ef38ef
Tyler Herndon <tylerherndon89@gmail.com>
2025-03-20 17:54:09 -0700

    Experimenting with printing multiple things when reading JSON
    Trying to refine how reading JSON file works

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function getJSONData(){
-	fetch('./data/stuff.json')
-                .then(response => { // Parse JSON Data
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-                    return response.json();  
-                })
-                .then(data => console.log(data.songs[0].id)) // Work with JSON Data
-                .catch(error => console.error('Failed to fetch data:', error)); 
+async function getJSONData(){
+    for (let i = 0; i < 2; i++){
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+        
+            const json = await response.json();
+            console.log(json.songs[i].name);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+	
 }
